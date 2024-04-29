0d921354b192da9acd421772dadb4bfbb886c382
Luigi Balestrucci <card3001@outlook.it>
2024-04-29 19:52:34 +0200

    Exercise SQL completed

---
+++ SQLite.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+	book_id INTEGER PRIMARY KEY,
+  	title TEXT NOT NULL,
+  	author TEXT NOT NULL,
+  	genre TEXT NOT NULL,
+  	published_year,
+  	isbn TEXT NOT NULL,
+  	price,
+  	rating,
+  	stock_count
+)
